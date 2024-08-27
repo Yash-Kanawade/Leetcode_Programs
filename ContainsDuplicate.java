a7e86853da44b0acdb6cefa89c7f5ea07d544ff5
Yash-Kanawade <yash.c.kanawade@gmail.com>
2024-08-27 08:41:21 +0530

    Q217-ADDED

---
+++ ContainsDuplicate.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 8, 5, 4, 4 };
+        boolean flag = false;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i] == nums[i - 1])
+                flag = true;
+        System.out.println(flag);
+    }
+}
